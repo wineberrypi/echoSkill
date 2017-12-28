5ad24025297d52a0103743f4b4328e469dc69350
bhaskarmelkani <melkani.bhaskar@gmail.com>
2017-12-28 20:00:23 +0100

    working

--- index.js
+++ index.js
@@ -4,28 +4,29 @@ const Alexa = require('alexa-sdk');
 const fetch = require('node-fetch');
 const apiUrl = 'http://54.194.56.149:4000/light/lamp/toggle';
 
-const APP_ID = 'amzn1.ask.skill.e9007884-6b33-4553-9a1f-80c0cf7265d9';
+const APP_ID = 'amzn1.ask.skill.c13732ea-714a-471e-89cf-aade219d5fb7';
 const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
 const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
+const SKILL_NAME = 'Dolores';
 
 const handlers = {
-    'LightIntent': () =>{
+    'LaunchRequest': function () {
+        this.emit('GetNewFactIntent');
+    },
+    'GetNewFactIntent': function (){
         const speechOutput = 'Light toggled';
+
         fetch(apiUrl)
             .then((res) => {
-                this
-                    .response
-                    .speak(speechOutput);
+                this.response.cardRenderer(SKILL_NAME, speechOutput);
+                this.response.speak(speechOutput);
                 this.emit(':responseReady');
             })
             .catch((err) => {
-                this
-                    .response
-                    .speak("Some error occured");
+                this.response.speak("Some error occured");
                 this.emit(':responseReady');
             });
-        
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = HELP_MESSAGE;
@@ -42,7 +43,6 @@ const handlers = {
         this.response.speak(STOP_MESSAGE);
         this.emit(':responseReady');
     }
-    
 }
 
 
