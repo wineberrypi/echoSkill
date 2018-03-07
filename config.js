9d69de42623baa61172840a51204f05cb430c077
bhaskarmelkani <melkani.bhaskar@gmail.com>
2018-03-07 13:42:53 +0100

    Initial Commit

---
+++ config.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  appId: 'amzn1.ask.skill.c13732ea-714a-471e-89cf-aade219d5fb7',  //APP id of skill
+  apiUrl: 'http://xx.xx.xx.xx:4000/light/lamp/toggle', //API url of server
+};
---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const Alexa = require('alexa-sdk');
+const fetch = require('node-fetch');
+const {apiUrl, appId} = require('./config');
+
+const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+const SKILL_NAME = 'Dolores';
+
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('LightIntent');
+    },
+    'LightIntent': function (){
+        const speechOutput = 'Light toggled';
+
+        fetch(apiUrl)
+            .then((res) => {
+                this.response.cardRenderer(SKILL_NAME, speechOutput);
+                this.response.speak(speechOutput);
+                this.emit(':responseReady');
+            })
+            .catch((err) => {
+                this.response.speak("Some error occured");
+                this.emit(':responseReady');
+            });
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    }
+}
+
+
+exports.handler = (event, context, callback) => {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.appId = appId;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
